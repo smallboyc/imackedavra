2ee8dec1f0cd60d0c17503ce63dc44faaa2fe147
Maxence Dupuis <104005395+smallboyc@users.noreply.github.com>
2023-11-30 14:36:43 +0100

    edit : quiz - add json file

--- quiz/index.js
+++ quiz/index.js
@@ -1,66 +1,5 @@
-const quiz = [
-  {
-    id: 0,
-    title: "Qui es-tu à l'imac?",
-    answers: [
-      { id: 0, answer: "Miss ou Mister Imac", house: "R" },
-      { id: 1, answer: "J'aurai dû être Miss ou Mister Imac", house: "V" },
-      { id: 2, answer: "Le gilet jaune", house: "J" },
-      { id: 3, answer: "Personne et je m'en branle", house: "B" },
-    ],
-  },
-  {
-    id: 1,
-    title: "Quelle est ta matière préférée ?",
-    answers: [
-      { id: 0, answer: "Pratique vidéo", house: "R" },
-      { id: 1, answer: "Le Web et l'Anglais", house: "J" },
-      { id: 2, answer: "Expression et écriture", house: "B" },
-      { id: 3, answer: "La programmation C++ et les Maths", house: "V" },
-    ],
-  },
-  {
-    id: 2,
-    title: "Vas-tu aux soirées de l’esiee?",
-    answers: [
-      { id: 0, answer: "Oui", house: ["R", "V"] },
-      { id: 1, answer: "Non", house: ["J", "B"] },
-    ],
-  },
-  {
-    id: 3,
-    title: "Tu viens de",
-    answers: [
-      { id: 0, answer: "MMI", house: "R" },
-      { id: 1, answer: "DUT Info ", house: "J" },
-      { id: 2, answer: "Prépa", house: "B" },
-      { id: 3, answer: "Autres", house: "V" },
-    ],
-  },
-  {
-    id: 4,
-    title: "As-tu fait ton gage de parrainage?",
-    answers: [
-      { id: 0, answer: "Oui en plus d’un mois", house: "R" },
-      { id: 1, answer: "Non et je le ferai sûrement pas", house: "V" },
-      { id: 2, answer: "Non pas encore mais c’est prévu", house: "J" },
-      { id: 3, answer: "Oui en moins d’un mois", house: "B" },
-    ],
-  },
-  {
-    id: 5,
-    title: "Pour les courts-métrage tu préfères t’occuper de ?",
-    answers: [
-      { id: 0, answer: "La réalisation ", house: "R" },
-      { id: 1, answer: "L’acting", house: "V" },
-      { id: 2, answer: "Le son", house: "J" },
-      { id: 3, answer: "La prise de vue", house: "B" },
-    ],
-  },
-];
-
-//On récupère la div principale : main
-const main = document.getElementById("quiz");
+//On récupère la div principale : quizContainer
+const quizContainer = document.getElementById("quiz");
 let table = [];
 let currentId = 0;
 
@@ -93,7 +32,7 @@ function selectedHouse(totalChoices) {
   result.sort((a, b) => b.value - a.value);
   const displayResult = document.createElement("p");
   displayResult.textContent = "Bravo tu es " + result[0].name;
-  main.appendChild(displayResult);
+  quizContainer.appendChild(displayResult);
   getAPI(
     `https://hp-api.onrender.com/api/characters/house/${result[0].api_value}`
   );
@@ -101,13 +40,13 @@ function selectedHouse(totalChoices) {
 }
 
 //Génère toutes les questions - réponses + Affiche la première question - réponses
-function loadQuiz() {
+function loadQuiz(quiz) {
   quiz.forEach((question) => {
     //Création et ajout de la DIV contenant la question et les réponses
     const questionDiv = document.createElement("div");
     questionDiv.classList.add("questionDiv");
     questionDiv.id = question.id;
-    main.appendChild(questionDiv);
+    quizContainer.appendChild(questionDiv);
     //Initialisation de la 1ère question
     if (questionDiv.id == currentId) {
       questionDiv.style.display = "block";
@@ -138,7 +77,7 @@ function loadQuiz() {
 }
 
 //On gère la partie jouée par l'utilisateur. (Clique + Nouvelles questions)
-function playQuiz() {
+function playQuiz(quiz) {
   const getAllAnswers = document.querySelectorAll("li");
   getAllAnswers.forEach((answer) => {
     answer.addEventListener("click", () => {
@@ -165,5 +104,12 @@ function playQuiz() {
   });
 }
 
-loadQuiz();
-playQuiz();
+//Fonction principale
+async function Quiz() {
+  const fetcher = await fetch("quiz.json");
+  const data = await fetcher.json();
+  loadQuiz(data);
+  playQuiz(data);
+}
+
+Quiz();
