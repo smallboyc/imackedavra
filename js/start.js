ed80d0054d6d284c23faa4195ede7988d582600a
Maxence Dupuis <104005395+smallboyc@users.noreply.github.com>
2023-12-09 23:59:49 +0100

    fix : random in start.js

--- js/start.js
+++ js/start.js
@@ -1,12 +1,12 @@
 //on récupère la balise <h1>
 const title = document.querySelector("h1");
-//on récupère le texte du <h1> et on split pour obtenir les letters dans un tableau.
+//on récupère le texte du <h1> et on split pour obtenir les lettres dans un tableau.
 const letters = title.innerText.split("");
-const setEffectToLetters = letters.map((lettre) => {
+const setEffectToLetters = letters.map((letter) => {
   //On génère un nombre aléatoire => animation aléatoire sur les lettres.
-  const randomNumber = Math.floor(Math.random() * 10);
-  if (randomNumber % 2 == 0)
-    return `<span class="oscillation_first">${lettre}</span>`;
-  else return `<span class="oscillation_second">${lettre}</span>`;
+  const randomNumber = Math.floor(Math.random() * 2);
+  if (randomNumber == 0)
+    return `<span class="oscillation_first">${letter}</span>`;
+  else return `<span class="oscillation_second">${letter}</span>`;
 });
 title.innerHTML = setEffectToLetters.join("");
